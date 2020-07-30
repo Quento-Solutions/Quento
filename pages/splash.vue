27babed5be4ba2f147bcb49e5ba70290a223e04c
Eshwar Chock <eshchock1@gmail.com>
2020-07-30 15:34:35 -0400

    splash page complete for hd resolution

---
+++ pages/splash.vue
@@ -0,0 +1,222 @@
+<template>
+ <div class="w-full" style="overflow-x:hidden;">
+      <div class="background">
+    </div>
+<section>
+   <div id="nav" style="position:absolute;height:4vw;top:0;left:0; width:100vw;display:flex;alignItems:center;margin-top:1vw;">
+      <a style="position:absolute;color:white;opacity:0.95;font-size:2vw;font-family:ginger-bold; left:8vw;">Quento</a>      
+      <a style="position:absolute;color:white;opacity:0.95;font-size:1vw;font-family:ginger-normal; right:8vw;background-color:rgba(255, 255, 255,0.3);border-radius:2vw;padding:0.35vw 2vw;">Sign in</a>      
+   </div>
+</section>
+
+      <section style="height:100vh;">
+          <div style="position:absolute;top:12vw;left:15vw;width:40vw;">
+              <h1 style="color:white;font-size:5vw;font-weight:bold;letter-spacing:-0.2vw;opacity:0.85;font-family:ginger-bold;line-height:5vw;">A collection of utilities created by <span style="color:#ff5c9f;opacity:1;">students</span>, for <span style="color:#ff5c9f;opacity:1;">students</span>.</h1>
+               <a style="color:white;font-size:1vw;font-family:ginger-normal;font-weight:bold;margin-top:1vw;display:inline-block;background:rgba(255,255,255,0.3);border-radius:2vw;padding:0.3vw 2vw;">Sign up for the BETA</a>
+               <a style="color:white;font-size:1vw;font-family:ginger-normal;font-weight:bold;margin-top:1vw;display:inline-block;padding:0.3vw 2vw;">Contact us</a>
+          </div>
+          <div class="animate-flicker" style="position:absolute;bottom:2vw;left:50%;transform:translate(-50%);text-align:center;">
+             <h1 style="color:white;font-size:1.4vw;font-family:ginger-normal;font-weight:bold;">Scroll Down</h1>
+             <img style="margin-top:1vw;width:2vw;margin-left:50%;transform:translate(-50%);" src="../assets/images/down.png">
+          </div>
+      </section>
+      <section style="height:100vh;background:#f6f9fc;width;100vw;display:flex;">
+         <div style="width:50%;">
+            <div style="margin-top:5vw;margin-left:10vw;">
+            <h1 style="color:#0a2540;font-family:ginger-bold;font-weight:bold;font-size:3vw;padding-top:1vw;letter-spacing:-0.1vw;">If only… no longer</h1>
+            <h1 style="color:#6b3deb;font-family:ginger-normal;font-weight:bold;font-size:1.5vw;padding-top:1vw;">The solution you've been waiting for is here.</h1>
+            <p style="color:#42547d;font-family:ginger-normal;font-size:1vw;padding-top:1vw;">Have you ever found yourself face-to-face with a task so daunting that you just exclaim,
+“Darn, if only there were only an app for that!”<br><br>As the high school students of today, we too-often find ourselves caught in an awkward transition period; one between the slow-to-change world of academia and our current age of rapidly-evolving technology. 
+</p>
+<a style="background-color:#6b3deb;color:white;border:none;border-radius:2vw;padding:0.3vw 2vw;font-family:ginger-normal;font-size:1vw;margin-top:1vw;display:inline-block;">Start your experience</a>
+
+
+            <h4 style="margin-top:4vw;color:#6b3deb;font-family:ginger-normal;font-weight:bold;font-size:1.1vw;">Revolutionary platform</h4>
+            <h2 style="color:#0a2540;font-family:ginger-normal;font-weight:bold;font-size:2vw;padding-top:1vw;">A fully integrated suite of digital tools</h2>
+            <p style="color:#42547d;font-family:ginger-normal;font-size:1vw;padding-top:1vw;">Quento is <span style="color:#6b3deb;font-weight:bold;">open-source</span>, freely available, and built from the ground up by a passionate community. Acting in the same capacity as a <span style="color:#6b3deb;font-weight:bold;">digital pencil case</span>, the platform serves to centralize a wide variety of <span style="color:#6b3deb;font-weight:bold;">web tools</span> created by and for <span style="color:#6b3deb;font-weight:bold;">Canadian high school students</span>.</p>
+            </div>
+         </div>
+          <div style="width:50%;display:flex;alignItems:center;justifyContent:center">
+            <img src="../assets/images/laptop.png" style="width:90%;"> 
+         </div>
+         
+      </section>
+      <section style="height:100vh;width:100vw; dispplay:flex;">
+         <div class="diagonal-background"></div>
+         <table style="height:100%;width:100%;margin:0;">
+            <tr style="width:100%;">
+               <td style="width:50%;"><table style="position:relative;width;100%;height:80%;margin-top:5vw;margin-left:8vw;">
+                  <tr>
+                     <td v-tilt="{reverse:'true', speed:1000,}" style="padding:2vw;transform-style:preserve-3d;"><div style="background-image:linear-gradient(to top right, #df43c6, #8c3af0);;height:17vw;width:12vw;border-radius:1vw;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);"><div class="bruh"><h1 style="color:#b13edd;background:white;width:3vw;height:3vw;font-size:2vw;font-family:arial;font-weight:bold;border-radius:10vw;text-align:center;padding-top:0.25vw;">1</h1><h3 style="color:white;font-family:ginger-normal;font-weight:bold;font-size:1vw;padding-top:1vw;">Easily convert written notes into a digital format</h3></div></div></td>
+                     <td v-tilt="{reverse:'true', speed:1000,}" style="padding:2vw;transform-style:preserve-3d;"><div style="background-image:linear-gradient(to top right, #df43c6, #8c3af0);;height:17vw;width:12vw;border-radius:1vw;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);"><div class="bruh"><h1 style="color:#b13edd;background:white;width:3vw;height:3vw;font-size:2vw;font-family:arial;font-weight:bold;border-radius:10vw;text-align:center;padding-top:0.25vw;">2</h1><h3 style="color:white;font-family:ginger-normal;font-weight:bold;font-size:1vw;padding-top:1vw;">Forward notifications from supported platforms<span style="color:gold;">*</span> as an SMS, Facebook, or Discord message</h3><p style="color:white;font-size:0.7vw;font-family:ginger-normal;margin-top:1vw;line-height:0.75vw;"><span style="color:gold">*</span> Managebac, Google Classroom, etc.</p></div></div></td>
+                   </tr>
+                   <tr>
+                     <td v-tilt="{reverse:'true', speed:1000,}" style="padding:2vw;transform-style:preserve-3d;"><div style="background-image:linear-gradient(to top right, #df43c6, #8c3af0);;height:17vw;width:12vw;border-radius:1vw;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);"><div class="bruh"><h1 style="color:#b13edd;background:white;width:3vw;height:3vw;font-size:2vw;font-family:arial;font-weight:bold;border-radius:10vw;text-align:center;padding-top:0.25vw;">3</h1><h3 style="color:white;font-family:ginger-normal;font-weight:bold;font-size:1vw;padding-top:1vw;">Create and join groups where you can create class calendars and plot out lessons</h3></div></div></td>
+                     <td v-tilt="{reverse:'true', speed:1000,}" style="padding:2vw;transform-style:preserve-3d;"><div style="background-image:linear-gradient(to top right, #df43c6, #8c3af0);;height:17vw;width:12vw;border-radius:1vw;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);"><div class="bruh"><h1 style="color:#b13edd;background:white;width:3vw;height:3vw;font-size:2vw;font-family:arial;font-weight:bold;border-radius:10vw;text-align:center;padding-top:0.25vw;">4</h1><h3 style="color:white;font-family:ginger-normal;font-weight:bold;font-size:1vw;padding-top:1vw;">Suggest feature ideas and provide feedback</h3></div></div></td>
+                  </tr>
+                   </table></td>
+               <td style="width:50%">
+            <div style="width:40vw;position:relative;">
+               <h1 style="color:white;font-family:ginger-bold;font-weight:bold;font-size:3vw;padding-top:1vw;letter-spacing:-0.1vw;">Our Vision</h1>
+                  <p style="color:white;font-family:ginger-normal;font-size:1vw;padding-top:1vw;">Faced with daily dilemmas such as “how should I take notes?” and “how do I stay on top of the ever-increasing workload?”, we established Quento Solutions as a platform to end these “darn!” moments once and for all. But how are we going to achieve this, you might ask? <br><br>Quento is currently in early stages of development. However, we need all the help that we can get. Scroll down below to check out how you can contribute and gain exclusive benefits as one of our earliest members and beta testers!</p>
+                 <a style="color:white;opacity:0.95;margin-top:1vw;display:inline-block;font-size:1vw;font-family:ginger-normal;background-color:rgba(255, 255, 255,0.3);border-radius:2vw;padding:0.35vw 2vw;">Learn how you can help</a>      
+         </div>
+         </td>
+            </tr>
+         </table>
+      </section>
+      <section style="background:#f6f9fc;width;100vw;display:flex; margin-top:5vw;">
+         <div style="width:50%;">
+            <div style="margin-top:5vw;margin-left:10vw;">
+            <h1 style="color:#0a2540;font-family:ginger-bold;font-weight:bold;font-size:2.5vw;padding-top:1vw;letter-spacing:-0.1vw;">I want to contribute as a Quento <span style="color:#6b3deb">beta tester</span>.</h1>
+            <p style="color:#42547d;font-family:ginger-normal;font-size:1vw;padding-top:1vw;">Great news! You can sign up for an account and subscribe to our newsletter today through using the forms below. Once you’ve signed in, you’ll have access to the first module that we have completed: a forum where interested users can post the features they want added next and upvote/downvote fellow recommendations! As our development team grows and Quento comes along in its production stages, we’ll pick the highest ranking recommendations to create next.</p>
+<a style="background-color:#6b3deb;color:white;border:none;border-radius:2vw;padding:0.3vw 2vw;font-family:ginger-normal;font-size:1vw;margin-top:1vw;display:inline-block;">Sign up for the beta</a>
+
+
+            <h1 style="color:#0a2540;font-family:ginger-bold;font-weight:bold;font-size:2.5vw;padding-top:1vw;letter-spacing:-0.1vw;margin-top:4vw;"> I want to contribute to the <span style="color:#6b3deb">development</span> of Quento.</h1>
+            <p style="color:#42547d;font-family:ginger-normal;font-size:1vw;padding-top:1vw;">Great news! You can sign up for an account and subscribe to our newsletter today through using the forms below. Once you’ve signed in, you’ll have access to the first module that we have completed: a forum where interested users can post the features they want added next and upvote/downvote fellow recommendations! As our development team grows and Quento comes along in its production stages, we’ll pick the highest ranking recommendations to create next.</p>
+<a style="background-color:#6b3deb;color:white;border:none;border-radius:2vw;padding:0.3vw 2vw;font-family:ginger-normal;font-size:1vw;margin-top:1vw;display:inline-block;">Join the development team</a>
+</div>
+         </div>
+          <div style="width:50%;margin-top:10vw;">
+             <h1 style="text-align:center;color:#6b3deb;font-family:ginger-bold;font-weight:bold;font-size:2.5vw;padding-top:1vw;letter-spacing:-0.1vw;margin-bottom:2vw;">Welcome to the beta</h1>
+            <form>
+                                <center><input style="font-family:ginger-normal, FontAwesome" class="textInput" placeholder=" Name" type="text" required></center><br>
+                                <center><input style="font-family:ginger-normal, FontAwesome" class="textInput" placeholder="  Email" type="email" required></center><br>
+                                <center><textarea style="font-family:ginger-normal, FontAwesome" placeholder="  Message" required></textarea></center><br>
+                                <center><input style="font-family:ginger-normal, FontAwesome" id="submitForm" type="submit" value="Send"></center>
+                            </form> 
+          <div style="display:flex;alignItems:center;justifyContent:center;margin-top:3vw;">
+          <img src="../assets/images/github.png" style="width:4vw;"> 
+             <img src="../assets/images/discord.png" style="width:4vw;margin-left:2vw;"> 
+             </div>
+         </div>
+        
+      </section>
+      <section style="margin-top:5vw;">
+         <div style="width:100vw;height:5vw; background-image: linear-gradient(to right, #4a45f4, #df43c6);display:flex;alignItems:center;">
+               <a style="position:absolute;color:white;opacity:0.95;font-size:2vw;font-family:ginger-bold; left:8vw;">Quento</a>      
+         </div>
+      </section>
+   </div>
+   </template>
+    <script src="https://kit.fontawesome.com/ed68982581.js" crossorigin="anonymous"></script>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import VueTilt from 'vue-tilt.js'
+Vue.use(VueTilt)
+
+@Component<Splash>({
+  components: {
+  },
+})
+export default class Splash extends Vue {
+    name="splash"
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes flickerAnimation {
+  0%   { opacity:0.85; }
+  50%  { opacity:0; }
+  100% { opacity:0.85; }
+}
+@-o-keyframes flickerAnimation{
+  0%   { opacity:0.85; }
+  50%  { opacity:0; }
+  100% { opacity:0.85; }
+}
+@-moz-keyframes flickerAnimation{
+  0%   { opacity:0.85; }
+  50%  { opacity:0; }
+  100% { opacity:0.85; }
+}
+@-webkit-keyframes flickerAnimation{
+  0%   { opacity:0.85; }
+  50%  { opacity:0; }
+  100% { opacity:0.85; }
+}
+.animate-flicker {
+   -webkit-animation: flickerAnimation 3s infinite;
+   -moz-animation: flickerAnimation 3s infinite;
+   -o-animation: flickerAnimation 3s infinite;
+    animation: flickerAnimation 3s infinite;
+}
+.textInput {
+   border:none;
+       padding-left:0.3vw;
+        width: 30vw;
+        height: 2.5vw;
+        font-size: 1vw;
+        border-radius: 0.4vw;
+        margin-top: 0;
+        color:#42547d;
+        background:#f6f9fc;
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.1);
+    }
+   textarea {
+         border:none;
+         box-shadow: 0 8px 16px 0 rgba(0,0,0,0.1);
+
+       padding-left:0.3vw;
+        width: 30vw;
+        height: 12vw;
+        font-size: 1vw;
+        border-radius: 0.4vw;
+        margin-top: 0;
+        color:#42547d;
+        background:#f6f9fc;
+   }
+   #submitForm {
+      background-color:#6b3deb;
+      color:white;
+      border:none;
+      border-radius:2vw;
+      padding:0.3vw;
+      width:20vw;
+      font-family:ginger-normal;
+      font-size:1vw;margin-top:1vw;
+      display:inline-block;
+      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.1);
+   }
+
+.bruh {
+   transform: translateZ(2vw);
+   padding-top:2vw;
+   width:80%;
+   margin-left: 10%;
+}
+
+   @font-face {
+  font-family: "ginger-bold";
+  src: url("../assets/font/HelveticaNowText-Bold.woff");
+}
+@font-face {
+  font-family: "ginger-normal";
+  src: url("../assets/font/HelveticaNowText-Regular.woff");
+}
+@font-face {
+  font-family: "ginger-light";
+  src: url("../assets/font/HelveticaNowText-Light.woff");
+}
+
+@import '../assets/scss/components/_main.scss';
+.background {
+    position:absolute;
+    height:100vh;
+    width:100vw;
+    background: url("../assets/images/background2.jpg") no-repeat;
+    background-size:100%;
+}
+
+.diagonal-background {
+   position:absolute;
+   background-color:#4a45f4;
+   background-image: linear-gradient(to right, #4a45f4, #df43c6);
+   height:50vw;
+   width:130vw;
+   transform: rotate(-8deg);
+   left:-20vw;
+}
+</style>
