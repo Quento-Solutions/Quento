bd655b2a9bd84561a1504902c66d55b909c3996b
Jeffrey Li <46302202+DrCheddar@users.noreply.github.com>
2020-08-07 14:17:13 -0400

    Jest Test Scripts (#22)
    * Posted notes (#18) (#19)
    
    * feat: responsive notes cards
    
    * feat : sorting sidebar
    
    * feat: add note modal without images or preview
    
    * feat: vsupload frontend side
    
    * preview notes page
    
    * feat : subject filtering
    
    * can like posts now
    
    * image upload works, file compression is int tho
    
    * feat : notes integrated, images are still wack as fuck
    
    * feat: really basic jest test

--- pages/auth/SignUp.vue
+++ pages/auth/SignUp.vue
@@ -13,7 +13,7 @@
             v-model="email"
             label="Email"
             color="primary"
-            placeholder="muhammad@ocdsb.ca"
+            placeholder="studentEmail@ocdsb.ca"
             class="block"
           >
             <template #icon>
