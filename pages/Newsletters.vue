7cb1bd6e16dd056f0a8e9c5b9f6abb148a6dacf1
Jeffrey Zhang <topcheese044@gmail.com>
2020-08-13 21:39:10 -0400

    family tree is a ladder time to fix

--- pages/Newsletters.vue
+++ pages/Newsletters.vue
@@ -2,20 +2,21 @@
   <div class="vx-row justify-center px-8">
     <h1 class="mb-12 text-title text-5xl mr-2">Newsletter Articles</h1>
     <div v-if="loaded">
-    <VxCard v-for="(item, index) in newsletters" :key="index" class="mb-6">
-      <div class="w-full text-ginger mb-20">
-        <h1>{{item.title}}</h1>
-        <div v-html="$md.render(item.content)" class="markdown-content"></div>
-      </div>
-      <!-- <div class="w-full text-ginger mb-20">
+      <VxCard v-for="(item, index) in newsletters" :key="index" class="mb-6 w-24 min-w-full">
+        <div class="w-full text-ginger mb-20">
+          <h1 class="text-center">{{item.title}}</h1>
+          <span class="mb-5">Author: {{item.Author}}</span>
+          <div v-html="$md.render(item.content)" class="md-container"></div>
+        </div>
+        <!-- <div class="w-full text-ginger mb-20">
         <h1 v-html="loaded? news[].title:''"></h1>
         <p v-html="loaded? $md.render(news[1].content):''"></p>
       </div>
       <div class="w-full text-ginger mb-20">
         <h1 v-html="loaded? news[2].title:''"></h1>
         <p v-html="loaded? $md.render(news[2].content):''"></p>
-      </div> -->
-    </VxCard>
+        </div>-->
+      </VxCard>
     </div>
   </div>
 </template>
@@ -24,7 +25,6 @@
 import { Vue, Component } from 'nuxt-property-decorator'
 import { newslettersStore } from '~/store'
 import VxCard from '~/components/VxCard.vue'
-
 
 @Component<NewslettersPage>({
   layout: 'main',
@@ -37,20 +37,19 @@ export default class NewslettersPage extends Vue {
   numberOfArticles = 3
   loaded = false
 
-  get newsletters()
-  {
-    return newslettersStore.newsletterList;
+  get newsletters() {
+    return newslettersStore.newsletterList
   }
 
   async GetNewsletters() {
     const self = this
-    const loading = this.$vs.loading();
+    const loading = this.$vs.loading()
     try {
-      await newslettersStore.GetNewsletters();
+      await newslettersStore.GetNewsletters()
     } catch (error) {
       console.log({ error })
     }
-    loading.close();
+    loading.close()
 
     this.loaded = true
     return
@@ -60,11 +59,66 @@ export default class NewslettersPage extends Vue {
 </script>
 
 <style lang="scss">
+.md-container {
+  font-family: 'Quicksand', sans-serif !important;
+  word-wrap: break-word;
 
-.markdown-content{
+  img {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
 
-  color : pink !important;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: 'ginger-bold', 'Trebuchet MS', 'Lucida Sans Unicode',
+      'Lucida Grande', 'Lucida Sans', Arial, sans-serif !important;
+    font-weight: 200 !important;
+    margin-bottom: 2rem !important;
+  }
+  p {
+    margin-bottom: 2rem !important;
+    font-weight: 500 !important;
+  }
+
+  blockquote {
+    margin-left: -10px !important;
+    padding-left: 23px !important;
+    box-shadow: rgb(41, 41, 41) 3px 0px 0px 0px inset !important;
+    p {
+      font-style: italic !important;
+    }
+  }
+  pre > code {
+    margin-bottom: 2rem !important;
+    background-color: #eeeeee;
+    border-radius: 0.5rem;
+    padding: 1rem;
+  }
+  ul,
+  ol {
+    li {
+      font-weight: 500 !important;
+    }
+    margin-left: 2rem;
+    margin-bottom: 1rem;
+    list-style-type: symbols();
+    p {
+      margin-bottom: 0.5rem !important;
+    }
+    ol {
+      list-style-type: lower-roman;
+    }
+
+    ul,
+    ol {
+      ol {
+        list-style-type: lower-alpha;
+      }
+    }
+  }
 }
-
-
 </style>
