3f3ba7a16e316e16b56e3492ea1f50c71a63388a
Jeffrey Li <dr.cheddar@outlook.com>
2020-08-14 11:45:15 -0400

    feat : questions content page

--- pages/questions/index.vue
+++ pages/questions/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <KeywordSelect label="label"></KeywordSelect>
+    <KeywordSelect label="label"></KeywordSelect>
     <vs-button @click="OpenPostQuestionModal()">Post New Question</vs-button>
     <ais-instant-search-ssr>
       <ais-search-box />
@@ -8,12 +8,14 @@
       <ais-refinement-list attribute="title" />
       <ais-hits>
         <template slot="item" slot-scope="{ item }">
-          <p>
-            <ais-highlight attribute="name" :hit="item" />
-          </p>
-          <p>
-            <ais-highlight attribute="title" :hit="item" />
-          </p>
+          <nuxt-link :to="`/questions/${item.objectID}`" @click="QuestionClicked(item)">
+            <p>
+              <ais-highlight attribute="contents" :hit="item" />
+            </p>
+            <p>
+              <ais-highlight attribute="title" :hit="item" />
+            </p>
+          </nuxt-link>
         </template>
       </ais-hits>
       <ais-pagination />
@@ -37,6 +39,7 @@ import {
 
 import algoliasearch from 'algoliasearch/lite'
 import { questionStore } from '~/store'
+import { Question, Question_t_A } from '~/types/questions'
 const searchClient = algoliasearch(algoliaConfig.appId, algoliaConfig.apiKey)
 
 @Component<QuestionsPage>({
@@ -48,7 +51,7 @@ const searchClient = algoliasearch(algoliaConfig.appId, algoliaConfig.apiKey)
     AisSearchBox,
     AisStats,
     AisPagination,
-    KeywordSelect,
+    KeywordSelect
   },
   serverPrefetch() {
     return this.instantsearch
@@ -79,10 +82,24 @@ const searchClient = algoliasearch(algoliaConfig.appId, algoliaConfig.apiKey)
 export default class QuestionsPage extends mixins(
   createServerRootMixin({ searchClient, indexName: 'dev_questions_index' })
 ) {
-  instantsearch!: any;
-  OpenPostQuestionModal()
+  instantsearch!: any
+  OpenPostQuestionModal() {
+    questionStore.SET_POST_MODAL_OPEN(true)
+  }
+  QuestionClicked(question: any) {
+    console.log({ question })
+  }
+
+    stringify(item : Question_t_A)
+    {
+        const a = new String(item.title);
+        console.log({a}, a.replaceAll);
+        
+        return encodeURI(a.replaceAll(" ", "-").replaceAll("?", ""));
+    }
+  AlgoliaConvert(item : Question_t_A)
   {
-      questionStore.SET_POST_MODAL_OPEN(true);
+      return Question.fromAlgolia(item);
   }
 }
 </script>
