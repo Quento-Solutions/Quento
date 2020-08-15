07abf68d11b031acc58ab5276a3b4c38f3b0c66c
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2020-08-14 21:45:37 -0400

    Added user's notes

---
+++ components/userCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-title>Card Title</v-card-title>
+    <v-card-subtitle>Secondary Title</v-card-subtitle>
+    <v-card-text>Some text</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'userCard'
+}
+</script>
