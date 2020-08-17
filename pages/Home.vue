c4e9f258262d74dbc0f625c4df8710209df84ec2
Jeffrey Li <dr.cheddar@outlook.com>
2020-08-17 11:36:04 -0400

    feat: home page rewrite without newsletter

--- pages/Home.vue
+++ pages/Home.vue
@@ -2,63 +2,99 @@
   <div id="home-page">
     <vs-row justify="center">
       <vs-col class="vx-col w-full">
-        <VxCard
-          class="text-center greet-user py-8"
-          title-color="#fff"
-          content-color="#fff"
-          :card-background="
-            'linear-gradient(120deg, rgba(91, 85, 215, 1.0), rgba(197, 68, 252, 0.5)), url(' +
-            require(`~/assets/images/homepageBackground.png`) +
-            ')'"
-        >
-        <template slot="no-body">
-          <div class="my-16 titleContainer">
-            <div>
-          <img
-            src="~/assets/images/QuentoLogoMain.png"
-            class="p-6 inline-flex rounded-full text-white center h-64"
-            alt="Logo"/>
-            </div>
-            <div>
-            <h1 class="mb-12 text-white text-title title">
-            QUENTO 
-          </h1>
-          </div>
-          </div>
+        <VxCard class="greet-user" content-color="#fff">
+          <template slot="no-body">
+            <div class="titleContainer vx-row w-full p-8">
+              <div class="vx-col w-full lg:w-1/4 sm:w-1/2" style="">
+                <img
+                  src="~/assets/images/quento_bird.png"
+                  class="p-6 inline-flex center w-full"
+                  alt="Logo"
+                />
+              </div>
 
-          <!-- <feather-icon icon="AwardIcon" class="p-6 mb-8 bg-primary inline-flex rounded-full text-white shadow" svgClasses="h-8 w-8"></feather-icon> -->
-          <div class="vx-row justify-center px-8">
+              <div
+                class="vx-col w-full md:w-1/2 p-8 flex flex-col justify-around"
+                style=""
+              >
+                <div class="text-title text-6xl text-black" style="">
+                  Title Content
+                </div>
+                <div class="text-title text-4xl text-black my-10" style="">
+                  Title Content
+                </div>
+                <div class="vx-row w-full justify-around" style="">
+                  <vs-tooltip
+                    v-for="(item, index) in contactInformation"
+                    :key="index"
+                  >
+                    <vs-avatar
+                      class="bottomIcon rounded-full cursor-pointer"
+                      style="width: 5vmax; height: 5vmax;"
+                      :color="item.backgroundColor"
+                      @click="pushWebsite(item.href)"
+                    >
+                      <i
+                        :class="item.iconClass"
+                        :style="`font-size: 4vmax; color: ${item.color}`"
+                        v-if="item.iconClass"
+                      ></i>
+                      <img v-else :src="item.imageUrl" />
+                    </vs-avatar>
+                    <template #tooltip>
+                      {{ item.toolTipText }}
+                    </template>
+                  </vs-tooltip>
+                </div>
+              </div>
 
-          <!-- <vs-avatar style="width : 5rem; height : 5rem;">
-              <img src="https://vignette.wikia.nocookie.net/leagueoflegends/images/6/6a/LoR_Cheers_Emote.png/revision/latest/top-crop/width/300/height/300?cb=20200429081948"/>
-            </vs-avatar> -->
-          </div>
-          
-          <vs-row justify="center" class="vx-row mt-10">
-            <div class="p-6 vx-row md:justify-end justify-center w-full md:w-1/2">
-              <vs-button
-                type="filled"
-                color="#aba6e0"
-                class="vx-col shadow-md  m-4 text-bold"
-                style="font-weight : bold"
-                size="xl"
-                href="https://docs.google.com/forms/d/e/1FAIpQLScrpPrTcpq41DJTXsmu1AvLOBXVHjG7vh2s_Q62M8oPx1ibmA/viewform?usp=sf_link"
-                >Subscribe To News Letter
-                </vs-button>
-            </div>
-            <div w=6 class="p-6 vx-row md:w-1/2 w-full md:justify-start justify-center">
-              <vs-button
-                color="#aba6e0"
-                class="vx-col shadow-md m-4 text-bold"
-                style="font-weight : bold"
-                size="xl"
-                @click="PushSuggestionsPage()"
+              <!-- Newsletter -->
+              <div class="vx-col w-full lg:w-1/4" style=""></div>
+
+              <!-- Videos -->
+              <div
+                class="text-ginger-b w-full p-4 text-3xl text-black"
+                style=""
+              >
+                Featured
+              </div>
+              <div class="vx-row w-full p-2 md:p-6" style="">
+                <swiper
+                  :options="swiperOption"
+                  :dir="$vs.rtl ? 'rtl' : 'ltr'"
+                  :key="$vs.rtl"
+                  style="width: 100%; height: 100%;"
                 >
-                Suggest Features
-              </vs-button>
+                
+                  <swiper-slide
+                    v-for="(item, index) in featuredContent"
+                    :key="index"
+                  >
+                    <div class="" style="padding-bottom: 56.25%;">
+                      <iframe
+                        style="
+                          width: 100%;
+                          height: 100%;
+                          position: absolute;
+                          left: 0px;
+                          top: 0px;
+                        "
+                        frameborder="0"
+                        width="100%"
+                        class="rounded-md"
+                        :src="item.videoUrl"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  </swiper-slide>
+                  <div class="swiper-pagination" slot="pagination"></div>
+                  <div class="swiper-button-prev" slot="button-prev"></div>
+                  <div class="swiper-button-next" slot="button-next"></div>
+                </swiper>
+              </div>
             </div>
-          </vs-row>
-        </template>
+          </template>
         </VxCard>
       </vs-col>
     </vs-row>
@@ -69,52 +105,141 @@
 import { Vue, Component } from 'nuxt-property-decorator'
 
 import { windowStore, authStore } from '~/store'
+import FooterCard from '~/components/FooterCard.vue'
 
-// import VxCard from '~/components/VxCard.vue'
+export interface ContactInformation {
+  name: string
+  href?: string
+  imageUrl: string
+  iconClass?: string
+  toolTipText?: string
+
+  backgroundColor?: string
+  color?: string
+  border?: boolean
+}
 
 @Component({
   layout: 'main',
-  components: {  }
+  components: { FooterCard }
 })
 export default class Home extends Vue {
-  pushComingSoon()
-  {
-    this.$router.push('/coming-soon');
+  pushComingSoon() {
+    this.$router.push('/coming-soon')
   }
-  PushSuggestionsPage()
-  {
-    this.$router.push("/suggestions");
+  PushSuggestionsPage() {
+    this.$router.push('/suggestions')
   }
+  contactInformation: ContactInformation[] = [
+    {
+      name: 'Github',
+      href: 'https://github.com/Quento-Solutions/Quento',
+      imageUrl: require(`~/assets/images/github-logo.png`),
+      iconClass: 'bx bxl-github',
+      toolTipText: 'https://github.com/Quento-Solutions/Quento',
+      backgroundColor: '#000000',
+      color: 'white'
+    },
+    {
+      name: 'Instagram',
+      imageUrl: require(`~/assets/images/QuentoLogoMain.png`),
+      toolTipText: 'Learn More About Quento',
+      iconClass: 'bx bxl-instagram',
+      backgroundColor: '#e1306c',
+      color: '#FFFFFF',
+      href: '/splash'
+    },
+    {
+      name: 'Discord',
+      imageUrl: require(`~/assets/images/github-logo.png`),
+      iconClass: 'bx bxl-discord',
+      toolTipText: 'Quento Discord Invite',
+      backgroundColor: '#7289DA',
+      color: '#FFFFFF',
+      border: true,
+      href: 'https://discord.gg/pfyFWus'
+    }
+  ]
+  swiperOption = {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    allowTouchMove: false,
+    lazy: true,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      }
+    }
+  }
+  featuredContent = [
+    {
+      name: 'Why is Physics so Hard?',
+      videoUrl: 'https://www.youtube.com/embed/qKa9ooHHcNw'
+    },
+    {
+      name: 'Why is Physics so Hard?',
+      videoUrl: 'https://www.youtube.com/embed/qKa9ooHHcNw'
+    },
+    {
+      name: 'Why is Physics so Hard?',
+      videoUrl: 'https://www.youtube.com/embed/qKa9ooHHcNw'
+    },
+        {
+      name: 'Why is Physics so Hard?',
+      videoUrl: 'https://www.youtube.com/embed/qKa9ooHHcNw'
+    },
+  ]
 
+  pushWebsite(website?: string) {
+    if (website) window.open(website)
+  }
 }
 </script>
 
 <style scoped>
 .title {
-font-family:ginger-bold;
-font-size:14rem;
+  font-family: ginger-bold;
+  font-size: 14rem;
 }
 .titleContainer {
-  display:flex;
+  display: flex;
   justify-content: center;
+  line-height: 1.125;
 }
 
 @media only screen and (max-width: 1200px) {
-.title {
-font-family:ginger-bold;
-font-size:10rem;
-}
-.titleContainer {
-  flex-direction: column;
-  align-items: center;
-
-}
+  .title {
+    font-family: ginger-bold;
+    font-size: 10rem;
+  }
+  .titleContainer {
+    flex-direction: column;
+    align-items: center;
+  }
 }
 
 @media only screen and (max-width: 767px) {
-.title {
-font-family:ginger-bold;
-font-size:15vw;
-}
+  .title {
+    font-family: ginger-bold;
+    font-size: 15vw;
+  }
 }
 </style>
