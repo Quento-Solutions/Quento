655cca5aaa58d98c5891275f5368170aca8a073e
Jeffrey Zhang <topcheese044@gmail.com>
2020-08-26 14:54:50 -0400

    error thrown back

--- store/auth.ts
+++ store/auth.ts
@@ -90,16 +90,6 @@ export default class AuthModule extends VuexModule implements AuthState {
     @Action({rawError : true})
     public async signUpWithEmail({email, password, displayName} : {email : string, password: string, displayName:string})
     {
-        // await firebaseAuth.createUserWithEmailAndPassword(email, password).catch(error => {
-        //     if (error.code === 'auth/email-already-in-use') {
-        //         console.log('in use')
-        //     }
-        
-        //     else if (error.code === 'auth/invalid-email') {
-        //         console.log('invalid email')
-        //     }
-        //     console.log(error);
-        //   }).then((userInfo:any) =>{ userInfo.user.updateProfile({displayName: displayName})})
         try
         {
             await firebaseAuth.createUserWithEmailAndPassword(email, password);
@@ -117,6 +107,7 @@ export default class AuthModule extends VuexModule implements AuthState {
                 console.log('invalid email')
             }
             console.log(error);
+            throw(error);
         }
     }
 
