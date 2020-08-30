31ce8aeadeb2132aa8b8b7a65f837418a36ddc14
Jeffrey Li <dr.cheddar@outlook.com>
2020-08-30 15:01:57 -0400

    fix: tooltip doesn't die error

---
+++ mixins/VsMixin.ts
@@ -0,0 +1,98 @@
+import Vue from 'vue'
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import { setColor } from '~/utils/vuesax';
+@Component
+export default class VsComponent extends Vue {
+  static install: (vue: any) => void
+
+  static use: (vue: any) => any
+
+  componentColor: string | null = null
+
+  @Prop({ type: String, default: null }) color!: string
+
+  @Prop({ type: Boolean, default: false }) danger!: boolean
+
+  @Prop({ type: Boolean, default: false }) success!: boolean
+
+  @Prop({ type: Boolean, default: false }) warn!: boolean
+
+  @Prop({ type: Boolean, default: false }) dark!: boolean
+
+  @Prop({ type: Boolean, default: false }) primary!: boolean
+
+  @Prop({ type: Boolean, default: false }) active!: boolean
+
+  get isColorDark() {
+    return this.color === 'dark' || this.dark || this.componentColor === 'dark'
+  }
+
+  changeColor() {
+    if (!this.$el || this.$el.nodeName == '#comment') { return }
+    this.componentColor = (this.danger && 'danger') ||
+    (this.success && 'success') ||
+    (this.warn && 'warn') ||
+    (this.dark && 'dark') ||
+    (this.primary && 'primary') || null;
+    if (this.color || this.componentColor) {
+      setColor('color', this.componentColor || this.color || 'primary', this.$el as HTMLElement, true)
+
+      if (this.$refs.options) {
+        setColor('color', this.componentColor || this.color || 'primary', this.$refs.options as HTMLElement, true)
+      }
+
+      if (this.$refs.tooltip) {
+        setColor('color', this.componentColor || this.color || 'primary', this.$refs.tooltip as HTMLElement, true)
+      }
+    }
+    if (this.componentColor == 'dark' || this.color == 'dark') {
+      this.$el.classList.add('vs-component-dark')
+    } else {
+      this.$el.classList.remove('vs-component-dark')
+    }
+
+    if (this.componentColor == 'white') {
+      this.$el.classList.add('vs-component-white')
+    } else {
+      this.$el.classList.remove('vs-component-white')
+    }
+  }
+
+  @Watch('color')
+  handleWatchColor() {
+    this.changeColor()
+  }
+
+  @Watch('primary')
+  handleWatchPrimary() {
+    this.changeColor()
+  }
+
+  @Watch('danger')
+  handleWatchDanger() {
+    this.changeColor()
+  }
+
+  @Watch('success')
+  handleWatchSuccess() {
+    this.changeColor()
+  }
+
+  @Watch('warn')
+  handleWatchWarn() {
+    this.changeColor()
+  }
+
+  @Watch('dark')
+  handleWatchDark() {
+    this.changeColor()
+  }
+
+  updated() {
+    this.changeColor()
+  }
+
+  mounted() {
+    this.changeColor()
+  }
+}
