f168f03b5411bc8dbbd59ac56051aa80d44fa709
Edwin Zheng <jessicawu414@gmail.com>
2020-09-01 19:29:15 -0400

    Fonts changes

--- components/NotesCard.vue
+++ components/NotesCard.vue
@@ -7,6 +7,7 @@
     @click="PushNotesPage()"
   >
     <DeleteNotesModal v-if="!disabled && NoteOwner" :open.sync="deleteNotesOpen" :noteId="note.id"></DeleteNotesModal>
+    
     <!-- Card Header -->
     <AvatarBar
       :username="note.userDisplayName"
@@ -38,10 +39,10 @@
         </div>
       </template>
     </AvatarBar>
+
+
     <!-- Category Pills -->
-    <div
-      class="w-4/5 vx-row p-2 items-start md:items-center text-sm mt-2 flex-col md:flex-row title-content"
-    >
+    <div class="w-4/5 vx-row p-2 items-start md:items-center text-sm mt-2 flex-col md:flex-row title-content">
       <div
         class="rounded-full bg-orange-500 p-2 px-4 vx-row items-center text-ginger text-white"
         style="background-color: #ed8936"
@@ -59,12 +60,14 @@
       </div>
     </div>
 
+
     <!-- Title -->
     <div
       class="w-4/5 text-ginger-b text-3xl p-4 ml-1/2 title-content"
       style="line-height: 1;"
     >{{ note.title }}</div>
 
+    
     <!-- Content -->
     <div class="vx-row w-full justify-center p-4 pt-0 m-0 pt-0" style="margin: 0;">
       <div
@@ -72,12 +75,15 @@
         :style="
           preview ? (hasImage ? 'max-height : 512px' : 'max-height: 200px') : ''"
       >
+        <!-- Image? -->
         <img
           :src="note.images[0]"
           class="responsive rounded border-solid mb-4"
           v-if="hasImage && preview"
           style="border-width: 2px; border-color: #ccd6dd;"
         />
+
+        <!-- Vue Flux -->
         <vue-flux
           :options="vfOptions"
           :images="note.images"
@@ -106,16 +112,19 @@
             <flux-index />
           </template>
         </vue-flux>
+
+        <!-- Actual Content -->
         <div
           v-html="$md.render(note.contents)"
           id="notes-md"
           class="w-full text-ginger p-2 md-container"
-        ></div>
+        />
+        <!-- And as quick as it started, it has finished -->
+
       </div>
     </div>
 
     <!-- Footer -->
-
     <div class="vx-row w-full justify-evenly lg:px-10 p-6">
       <vs-avatar
         class="icon-small"
@@ -148,138 +157,148 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'nuxt-property-decorator'
-import { Note } from '~/types/notes'
-import { SubjectIconList, SubjectGroup_O, Subject_O } from '~/types/subjects'
-import { notesStore, authStore } from '~/store'
-import DeleteNotesModal from '~/components/DeleteNotesModal.vue'
-import AvatarBar from '~/components/AvatarBar.vue'
+  // Imports
+  import { Component, Vue, Prop } from 'nuxt-property-decorator'
+  import { Note } from '~/types/notes'
+  import { SubjectIconList, SubjectGroup_O, Subject_O } from '~/types/subjects'
+  import { notesStore, authStore } from '~/store'
+  import DeleteNotesModal from '~/components/DeleteNotesModal.vue'
+  import AvatarBar from '~/components/AvatarBar.vue'
 
-@Component<NotesCard>({
-  components: { DeleteNotesModal, AvatarBar },
-  mounted() {
-    if (this.note.images && this.note.images.length) {
-      this.image = new Image()
-      this.image.src = this.note.images[0]
-      // this.note.userP
-      this.hasImage = true
+  @Component<NotesCard>({
+    components: { DeleteNotesModal, AvatarBar },
+    mounted() {
+      if (this.note.images && this.note.images.length) {
+        this.image = new Image()
+        this.image.src = this.note.images[0]
+        // this.note.userP
+        this.hasImage = true
+      }
     }
-  }
-})
-export default class NotesCard extends Vue {
-  @Prop({ required: true }) note!: Note
-  @Prop({ default: false }) clickable!: boolean
-  // Loaded full content
-  @Prop({ default: false }) preview!: boolean
-  // Whether buttons should work or not
-  @Prop({ default: false }) disabled!: boolean
+  })
+  
+  // OUTPUT
+  export default class NotesCard extends Vue {
+    @Prop({ required: true }) note!: Note
+    @Prop({ default: false }) clickable!: boolean
+    // Loaded full content
+    @Prop({ default: false }) preview!: boolean
+    // Whether buttons should work or not
+    @Prop({ default: false }) disabled!: boolean
 
-  deleteNotesOpen = false
+    deleteNotesOpen = false
 
-  OpenDeleteModal() {
-    this.deleteNotesOpen = true
-  }
-  get NoteOwner() {
-    return authStore.user?.uid == this.note.uid
-  }
+    OpenDeleteModal() {
+      this.deleteNotesOpen = true
+    }
 
-  OpenEditingModal() {
-    notesStore.SetEditNote(this.note)
-  }
+    get NoteOwner() {
+      return authStore.user?.uid == this.note.uid
+    }
 
-  getIcon(subject: SubjectGroup_O | Subject_O) {
-    return SubjectIconList[subject]
-  }
-  userLiked(id?: string) {
-    if (!id) return false
-    return notesStore.likedPosts.includes(id)
-  }
+    OpenEditingModal() {
+      notesStore.SetEditNote(this.note)
+    }
 
-  async toggleLike(id?: string, time?: any) {
-    if (!id || this.disabled) return
-    const a = this.$vs.loading()
-    await notesStore.ToggleLikedNote(id)
-    a.close()
+    getIcon(subject: SubjectGroup_O | Subject_O) {
+      return SubjectIconList[subject]
+    }
+
+    userLiked(id?: string) {
+      if (!id) return false
+      return notesStore.likedPosts.includes(id)
+    }
+
+    async toggleLike(id?: string, time?: any) {
+      if (!id || this.disabled) return
+      const a = this.$vs.loading()
+      await notesStore.ToggleLikedNote(id)
+      a.close()
+    }
+
+    vfOptions = {
+      autoplay: false,
+      allowFullscreen: true
+    }
+
+    vfTransitions = ['swipe']
+
+    PushNotesPage() {
+      if (this.clickable) return this.$router.push(`/notes/${this.note.id}`)
+    }
+
+
+    hasImage = false
+    image?: HTMLImageElement
   }
-  vfOptions = {
-    autoplay: false,
-    allowFullscreen: true
-  }
-  vfTransitions = ['swipe']
-  PushNotesPage() {
-    if (this.clickable) return this.$router.push(`/notes/${this.note.id}`)
-  }
-  hasImage = false
-  image?: HTMLImageElement
-}
 </script>
 <style lang="scss">
-#note-card {
-  .vs-navbar__group__items {
-    min-width: 120px;
-    background-color: white;
-    .menu-item:hover {
-      opacity: 1;
-      background-color: initial !important;
-    }
-    .menu-item {
-      opacity: 0.8;
-      transition-duration: 0.1s;
-      cursor: pointer;
-    }
-  }
-  .icon {
-    width: 4rem !important;
-    height: 4rem !important;
-    max-width: 10vw;
-    max-height: 10vw;
-  }
-  .icon-small {
-    width: 3rem !important;
-    height: 3rem !important;
-    max-width: 8vw;
-    max-height: 8vw;
-  }
-  #profile {
+  #note-card {
     .vs-navbar__group__items {
-      min-width: 0 !important;
-      // max-width : 100%;
-      width: 150%;
-      transform: translate(-25%, 90%);
+      min-width: 120px;
+      background-color: white;
+      .menu-item:hover {
+        opacity: 1;
+        background-color: initial !important;
+      }
+      .menu-item {
+        opacity: 0.8;
+        transition-duration: 0.1s;
+        cursor: pointer;
+      }
+    }
+    .icon {
+      width: 4rem !important;
+      height: 4rem !important;
+      max-width: 10vw;
+      max-height: 10vw;
+    }
+    .icon-small {
+      width: 3rem !important;
+      height: 3rem !important;
+      max-width: 8vw;
+      max-height: 8vw;
+    }
+    #profile {
+      .vs-navbar__group__items {
+        min-width: 0 !important;
+        // max-width : 100%;
+        width: 150%;
+        transform: translate(-25%, 90%);
+      }
     }
   }
-}
-.previewClickable {
-  :hover {
-    background-color: #f5f5f6;
+  .previewClickable {
+    :hover {
+      background-color: #f5f5f6;
+    }
+    transition-duration: 100ms !important;
   }
-  transition-duration: 100ms !important;
-}
-::-webkit-scrollbar {
-  height: 10px;
-}
+  ::-webkit-scrollbar {
+    height: 10px;
+  }
 
-::-webkit-scrollbar-thumb {
-  background: rgba(90, 90, 90, 0.5);
-  border-radius: 15px !important;
-}
+  ::-webkit-scrollbar-thumb {
+    background: rgba(90, 90, 90, 0.5);
+    border-radius: 15px !important;
+  }
 
-::-webkit-scrollbar-track {
-  background: rgba(90, 90, 90, 0.1);
-  border-radius: 15px !important;
-}
-.title-content {
-  margin-left: 10%;
-}
-@media only screen and (max-width: 768px) {
+  ::-webkit-scrollbar-track {
+    background: rgba(90, 90, 90, 0.1);
+    border-radius: 15px !important;
+  }
   .title-content {
-    margin-left: 0%;
+    margin-left: 10%;
   }
-}
-.flux-image {
-  background-position: 0px 0px !important;
-  background-size: auto 100% !important;
-  background-color: black;
-  background-position: center !important;
-}
+  @media only screen and (max-width: 768px) {
+    .title-content {
+      margin-left: 0%;
+    }
+  }
+  .flux-image {
+    background-position: 0px 0px !important;
+    background-size: auto 100% !important;
+    background-color: black;
+    background-position: center !important;
+  }
 </style>
