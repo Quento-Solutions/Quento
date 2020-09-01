06d5d4d6f6fc80c89383b34782069bccfcc0957b
Jeffrey Li <dr.cheddar@outlook.com>
2020-09-01 13:36:53 -0400

    fix: invalid date algolia error

--- types/firebaseTypes.ts
+++ types/firebaseTypes.ts
@@ -3,15 +3,15 @@ export interface Date_t_F
     seconds : number;
     nanoseconds : number
 }
-export interface Date_t_A
+export type Date_t_A =
 {
     _seconds : number;
     _nanoseconds : number
-}
+} | string;
 export interface StoredImage
 {
     fileName : string;
     imageURL : string;
 }
 export const firebaseDate = (date : Date_t_F) => date ? new Date(date.seconds * 1000) : new Date(0);
-export const algoliaDate = (date : Date_t_A) => new Date(date._seconds * 1000);
+export const algoliaDate = (date : Date_t_A) => new Date((date as string));
