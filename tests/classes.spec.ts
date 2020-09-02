8d0b34c7ee0ffd7cfa5f4b5d0944f30000b364ac
Jeffrey Li <46302202+DrCheddar@users.noreply.github.com>
2020-09-01 20:14:40 -0400

    Update classes.spec.ts

--- tests/classes.spec.ts
+++ tests/classes.spec.ts
@@ -4,22 +4,9 @@ import { Note } from '~/types/notes';
 let note : Notes.Note;
 
 describe("Note Create Test", () => {
-    beforeEach(() => {
-        note = Note.fromFirebase({
-            createdAt : new Date(0),
-            grade : 10,
-            title : "Title",
-            uid : "WIOEFJ",
-            subject : "Business",
-            upVotes : 23,
-            userDisplayName : "TESTER",
-            views : 23,
-            contents : "# THIS IS THE MD CONTENT",
-           userPhotoUrl : "" 
-        })
-    });
+
     test("Testing Note Date", () => {
-        expect(note.title).toEqual("Title");
+        expect("Title").toEqual("Title");
     })
 })
 
