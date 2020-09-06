93e907f0304cd52a53af4ae6d544175bef3b6bca
Eshwar Chock <eshchock1@gmail.com>
2020-09-06 18:04:32 -0400

    groups id page redesign

--- pages/groups/g/_id/index.vue
+++ pages/groups/g/_id/index.vue
@@ -3,12 +3,12 @@
           <input type="text" :value='JoinToken' id="joinInput" style="position : absolute; z-index : -10000000"/>
 
     <div class="vx-row justify-center w-full" v-if="group">
-      <VxCard class="mb-6 w-24 min-w-full">
+      <VxCard class="mb-6 w-24 min-w-full" style="border-radius: 0.8rem;">
         <template slot="no-body">
           <div
             class="text-white flex flex-col background-cover p-4 md:p-8 w-full"
             :style="backgroundGradient(group.backgroundImageUrl)"
-            style="min-height: 20vw;"
+            style="min-height: 20vw;border-radius: 0.6rem;"
           >
             <input
               class="text-white text-title text-6xl font-medium"
@@ -19,8 +19,8 @@
           </div>
         </template>
       </VxCard>
-      <div class="w-full vx-row bg-white rounded-md">
-        <div class="vx-row mb-4 w-full justify-around p-2 border-b-2 border-black">
+      <div class="w-full vx-row">
+        <div class="vx-row mb-8 w-full bg-white justify-between p-4 pr-24 pl-12" style="border-radius: 0.375rem;">
           <vs-avatar class="icon-small float-right" @click="goBack()">
             <i class="bx bx-arrow-back" style="font-size: 1.25rem;" />
           </vs-avatar>
@@ -38,12 +38,15 @@
         </div>
         <!-- Summary -->
 
-        <div class="w-full md:w-2/3 p-4 md:px-8 content-input">
-          <h1 class="text-title">Description</h1>
-          <div class="mt-4">{{group.description}}</div>
-
-          <h1 class="text-title mt-6">Join Link</h1>
-          <vs-button @click="GenerateJoinToken()">Generate Join Link</vs-button>
+        <div class="w-full md:w-2/3 p-4 md:px-8 content-input bg-white" style="border-radius: 0.375rem;">
+          <h1 class="text-ginger-b">Description</h1>
+          <div class="mt-4 text-ginger">{{group.description}}</div>
+          <div class="mt-4 text-ginger"><b>Date Created:</b> {{group.createdAt.toDateString().slice(4, 10) + ',' + group.createdAt.toDateString().slice(10)}}</div>
+          <div class="mt-4 text-ginger"><b>Grade:</b> {{group.grade}}</div>
+          <div class="mt-4 text-ginger"><b>Subject:</b> {{group.subject}}</div>
+          <div class="mt-4 text-ginger"><b>School:</b> {{group.school}}</div>
+          <div class="flex flex-row items-center my-6">
+          <h1 class="text-ginger-b mr-6">Join Link</h1><vs-button @click="GenerateJoinToken()" class="text-ginger">Generate Join Link</vs-button></div>
           <vs-input
             class="mt-4 block"
             readonly
@@ -79,8 +82,12 @@
 
         <!-- Member List -->
         <div class="vx-row justify-center w-full md:w-1/3">
-          <div class="vx-row w-full bg-white rounded-md lg:w-1/2">
-            <div class="p-4 md:p-6 lg:p-8">test</div>
+          <div class="vx-row w-full bg-white rounded-md lg:w-2/3" style="border-radius: 0.375rem;">
+            <div class="w-full pl-6 p-4 pt-4 pb-2 text-ginger text-2xl"><span class="text-ginger-b mb-6">Members ({{group.memberList.length}}):</span>
+            <div v-for="(user, index) in members" :key="index" class="flex flex-row items-center text-ginger text-xl my-4">
+              <img :src="user.photoUrl" class="w-12 h-12 mr-4" style="border-radius:0.5rem;">{{user.displayName}}
+            </div>
+            </div>
           </div>
         </div>
       </div>
@@ -108,10 +115,10 @@ import {Group, Group_t_F} from '~/types/groups'
   }
 })
 export default class GroupsSummary extends mixins(UserMixin) {
+  members: any[] = [];
   groupId: string | null = null
   docNotFound = false
   group: Group | null = null
-
   get JoinToken() {
     if (this.group?.inviteToken) {
       const redirect_uri = window.location.origin
@@ -122,6 +129,7 @@ export default class GroupsSummary extends mixins(UserMixin) {
   active: 'Questions' | 'Summary' | 'Notes' = 'Summary'
 
   backgroundGradient(imageUrl: string) {
+    this.fetchMembers()
     return `background-image : linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('${imageUrl}')`
   }
   goBack() {
@@ -175,6 +183,16 @@ export default class GroupsSummary extends mixins(UserMixin) {
       color : "success"
     })
   }
+
+  async fetchMembers() {
+    const userDocs = await Promise.all(this.group.memberList.map(id => firestore.collection('users').doc(id).get()))
+    userDocs[0].data()
+    const bruh = userDocs.map(doc => doc.data())
+// console.log(bruh[0].displayName)
+    this.members = bruh 
+// const userData = userDocs.map(doc => ...doc.data(), id : doc.id})
+  }
+
   async fetchGroup() {
     //   this.group!.bac
     const loading = this.$vs.loading()
