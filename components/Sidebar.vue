1fe2cbdc945d99034733d5e3e14199175aee9732
Jeffrey Li <dr.cheddar@outlook.com>
2020-09-07 12:12:48 -0400

    feat: rename newsletters on sidebar to updates

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,82 +1,73 @@
 <template>
+  <vs-sidebar
+    fixed
+    :hover-expand="hover"
+    :reduce="reduce"
+    v-model="active"
+    :open.sync="open"
+    style="z-index: 100000;"
+    color="#9f7aea"
+    id="sidebar"
+  >
+    <template #logo>
+      <vs-sidebar-item>
+        <template #icon>
+          <img
+            :src="require(`~/assets/images/QuentoLogoMain.png`)"
+            style="width: 2.7rem; height: 3rem;"
+          />
+        </template>
+        <div style>QUENTO</div>
+      </vs-sidebar-item>
+    </template>
+    <vs-sidebar-item v-for="(item, index) in NavOptions" :key="index" :id="item.href">
+      <template #icon>
+        <i class="sidebar-icon bx" :class="item.icon"></i>
+      </template>
+      {{ item.name }}
+    </vs-sidebar-item>
 
-    <vs-sidebar
-      fixed
-      :hover-expand="hover"
-      :reduce="reduce"
-      v-model="active"
-      :open.sync="open"
-      style="z-index: 100000;"
-      color="#9f7aea"
-      id="sidebar"
-    >
-      <template #logo>
-        <vs-sidebar-item>
+    <vs-sidebar-item href="https://github.com/Quento-Solutions">
+      <template #icon>
+        <i class="sidebar-icon bx bxl-github"></i>
+      </template>
+      <div style>Github</div>
+    </vs-sidebar-item>
+
+    <vs-sidebar-item href="https://discord.gg/vHhtD38">
+      <template #icon>
+        <i class="sidebar-icon bx bxl-discord"></i>
+      </template>
+      <div style>Discord</div>
+    </vs-sidebar-item>
+
+    <vs-sidebar-group>
+      <template #header>
+        <vs-sidebar-item arrow>
           <template #icon>
-            <img
-              :src="require(`~/assets/images/QuentoLogoMain.png`)"
-              style="width: 2.7rem; height: 3rem;"
-            />
+            <i class="bx bx-shape-polygon sidebar-icon"></i>
           </template>
-          <div style="">QUENTO</div>
+          Coming Soon
         </vs-sidebar-item>
       </template>
-      <vs-sidebar-item
-        v-for="(item, index) in NavOptions"
-        :key="index"
-        :id="item.href"
-      >
+
+      <vs-sidebar-item v-for="(item, index) in ComingSoonGroup" :key="index" :id="item.href">
         <template #icon>
           <i class="sidebar-icon bx" :class="item.icon"></i>
         </template>
         {{ item.name }}
       </vs-sidebar-item>
+    </vs-sidebar-group>
 
-      <vs-sidebar-item href="https://github.com/Quento-Solutions">
-        <template #icon>
-          <i class="sidebar-icon bx bxl-github"></i>
-        </template>
-        <div style="">Github</div>
-      </vs-sidebar-item>
-
-      <vs-sidebar-item href="https://discord.gg/vHhtD38">
-        <template #icon>
-          <i class="sidebar-icon bx bxl-discord"></i>
-        </template>
-        <div style="">Discord</div>
-      </vs-sidebar-item>
-
-      <vs-sidebar-group>
-        <template #header>
-          <vs-sidebar-item arrow>
-            <template #icon>
-              <i class="bx bx-shape-polygon sidebar-icon"></i>
-            </template>
-            Coming Soon
-          </vs-sidebar-item>
-        </template>
-
-        <vs-sidebar-item
-          v-for="(item, index) in ComingSoonGroup"
-          :key="index"
-          :id="item.href"
-        >
-          <template #icon>
-            <i class="sidebar-icon bx" :class="item.icon"></i>
-          </template>
-          {{ item.name }}
-        </vs-sidebar-item>
-      </vs-sidebar-group>
-
-      <template #footer> </template>
-    </vs-sidebar>
+    <template #footer></template>
+  </vs-sidebar>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+import {Vue, Component} from 'nuxt-property-decorator'
 
-import { navigationStore, windowStore, authStore } from '~/store'
-import { NavigationOptionType } from '~/store/navigation'
+import {navigationStore, windowStore, authStore} from '~/store'
+import {NavigationOptionType} from '~/store/navigation'
 
 @Component
 export default class Sidebar extends Vue {
@@ -108,24 +99,17 @@ export default class Sidebar extends Vue {
       href: '/questions'
     },
     {
-      name: 'Newsletter',
-      id: 'newsletters',
-      icon: 'bx bxs-envelope-open',
-      href: '/newsletters'
-    },
-    {
-      name: 'Suggestions',
-      id: 'suggestions',
-      icon: 'bx bxs-message-square-edit',
-      href: '/suggestions'
-    },
-    {
       name: 'Groups',
       id: 'groups',
       icon: 'bx bxs-group',
       href: '/groups'
+    },
+    {
+      name: 'Updates',
+      id: 'newsletters',
+      icon: 'bx bxs-envelope-open',
+      href: '/newsletters'
     }
-
   ] as const
 
   ComingSoonGroup = [
