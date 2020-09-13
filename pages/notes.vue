5c76e978e63c2b852a084739540dc3a8f921450b
Jeffrey Li <dr.cheddar@outlook.com>
2020-09-13 17:47:08 -0400

    fix: make edit notes good

--- pages/notes.vue
+++ pages/notes.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
     <PreviewNotesModal></PreviewNotesModal>
-    <EditNotesModal></EditNotesModal>
     <PostNotesModal></PostNotesModal>
 
     <nuxt-child></nuxt-child>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue, Prop, mixins } from 'nuxt-property-decorator'
+import {Component, Vue, Prop, mixins} from 'nuxt-property-decorator'
 
-import EditNotesModal from '~/screens/notes/EditNotesModal.vue'
 import PreviewNotesModal from '~/screens/notes/PreviewNotesModal.vue'
 import PostNotesModal from '~/screens/notes/PostNotesModal.vue'
 import AnalyticsMixin from '~/mixins/AnalyticsMixin'
 
-@Component<NotesGroup>({ components: {
-EditNotesModal, PreviewNotesModal, PostNotesModal
-}, layout: 'main', name : "NotesGroup" })
-export default class NotesGroup extends mixins(AnalyticsMixin) 
-{
-  logPage = true;
+@Component<NotesGroup>({
+  components: {
+    PreviewNotesModal,
+    PostNotesModal
+  },
+  layout: 'main',
+  name: 'NotesGroup'
+})
+export default class NotesGroup extends mixins(AnalyticsMixin) {
+  logPage = true
 }
 </script>
--- pages/notes/_id.vue
+++ pages/notes/_id.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="vx-row w-full justify-center" style="">
-    <div class="vx-row w-full md:w-2/3 p-4" style="">
-      <div class="vx-col w-full display-none md:inline-flex" style="">
+  <div class="vx-row w-full justify-center" style>
+    <EditNotesModal :isActive.sync="Editing" :NoteData="note" @reset="noteIdChange()"></EditNotesModal>
+    <div class="vx-row w-full md:w-2/3 p-4" style>
+      <div class="vx-col w-full display-none md:inline-flex" style>
         <div class="vx-row mb-4 w-full bg-white rounded-md p-2">
           <vs-avatar class="icon-small float-right" @click="goBack()">
             <i class="bx bx-arrow-back" style="font-size: 1.25rem;" />
           </vs-avatar>
         </div>
       </div>
-      <NotesCard :note="note" v-if="note"> </NotesCard>
+      <NotesCard :note="note" v-if="note"></NotesCard>
       <vs-alert color="danger" v-if="docNotFound">
-        <template #title>
-          Something Went Wrong
-        </template>
+        <template #title>Something Went Wrong</template>
         <b>Sorry!</b> Something went wrong when fetching the Note. Please Try
         Again.
       </vs-alert>
@@ -20,37 +19,55 @@
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue, Prop, Watch } from 'nuxt-property-decorator'
+import {Component, Vue, Prop, Watch, mixins} from 'nuxt-property-decorator'
 import NotesCard from '~/components/NotesCard.vue'
-import { Note, Note_t_F } from '~/types/notes'
+import {Note, Note_t_F} from '~/types/notes'
 import firestore from '~/plugins/firestore'
-import { notesStore } from '~/store'
+import {notesStore} from '~/store'
+import EditNotesModal from '~/screens/notes/EditNotesModal.vue'
+import UserMixin from '~/mixins/UserMixin'
+
 @Component<NotesContentPage>({
-  components: { NotesCard },
+  components: {NotesCard, EditNotesModal},
   layout: 'main',
   mounted() {
-    this.fetchNotes()
+    this.noteIdChange()
   }
 })
-export default class NotesContentPage extends Vue {
+export default class NotesContentPage extends mixins(UserMixin) {
   note: Note | null = null
-  noteId: string | null = null
   docNotFound = false
-  
-  get NoteId()
+
+  get Editing()
   {
-    return this.$route.params.id;
+    return this.$route.query.editing === 'true';
   }
-  @Watch("NoteId")
-  noteIdChange(value : string, oldValue : string)
+  set Editing(val : boolean)
   {
+    this.$router.replace(`/notes/${this.noteId}${val ? '?editing=true' : ''}`)
+  }
+
+  @Watch('Editing')
+  editingChange(newVal ?: boolean, oldVal ?: boolean)
+  {
+    if(!this.AuthUser?.uid || (this.note && this.AuthUser.uid !== this.note.uid))
+    {
+      this.Editing = false;
+    }
+  }
+
+  get noteId() {
+    return this.$route.params.id
+  }
+  @Watch('noteId')
+  async noteIdChange(value ?: string, oldValue ?: string) {
     // This is to update the page when going from notes/note1 -> notes/note2
-    this.fetchNotes();
+    await this.fetchNotes()
+    this.editingChange(this.Editing);
   }
 
   async fetchNotes() {
-    const loading = this.$vs.loading();
-    this.noteId = this.$route.params.id;
+    const loading = this.$vs.loading()
     if (!this.noteId) {
       this.$router.push('/notes')
       return
@@ -63,14 +80,14 @@ export default class NotesContentPage extends Vue {
         loading.close()
         return
       }
-      notesStore.IncrementView(this.noteId);
+      notesStore.IncrementView(this.noteId)
       this.note = Note.fromFirebase(noteData, doc.id)
       //Add note to activeNotes so functions work
       notesStore.pushActiveNotes(this.note)
       loading.close()
       return
     } catch (error) {
-      console.log({ error })
+      console.log({error})
       loading.close()
     }
   }
